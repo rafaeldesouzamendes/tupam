84ee5ff6f283d7a3efba3d1c9289360eeda63e54
rafaeldesouzamendes <rafaeldesouzamendes@gmail.com>
2013-05-23 15:35:50 -0300

    Adicionado SNMP4J

--- Tupam/src/main/java/br/ufsc/tupam/monitoring/controller/MonitoringController.java
+++ Tupam/src/main/java/br/ufsc/tupam/monitoring/controller/MonitoringController.java
@@ -1,8 +1,14 @@
 package br.ufsc.tupam.monitoring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/monitoring")
 public class MonitoringController {
 
+	@RequestMapping("/list")
+	public String listMonitoring(){
+		return "monitoring/index";
+	}
 }
