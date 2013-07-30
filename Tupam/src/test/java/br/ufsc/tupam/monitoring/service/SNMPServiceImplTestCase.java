5598698991d42d4afeecebd2b73c643a68635474
Rafael <Rafael@Rafael-XPS-14>
2013-07-30 14:43:06 -0300

    Alterações para converter o projeto para utilizar maven

--- Tupam/src/test/java/br/ufsc/tupam/monitoring/service/SNMPServiceImplTestCase.java
+++ Tupam/src/test/java/br/ufsc/tupam/monitoring/service/SNMPServiceImplTestCase.java
@@ -1,24 +1,20 @@
 package br.ufsc.tupam.monitoring.service;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import br.ufsc.tupam.monitoring.service.ISNMPService;
-import br.ufsc.tupam.planning.service.IPlanningService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SNMPServiceImplTestCase {
 
 	@Autowired
 	private ISNMPService service;
-	
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Assert.fail("Not yet implemented");
 	}
 
 }
